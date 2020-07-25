721a91b00dee74a22fbd39ee31925e8ddfdb2028
reality77 <david.d77@protonmail.com>
2020-07-25 19:11:27 +0200

    Fix migration

--- src/dal/Migrations/20200516090121_transaction_balance_to_table.cs
+++ src/dal/Migrations/20200516090121_transaction_balance_to_table.cs
@@ -14,7 +14,7 @@ namespace dal.Migrations
             migrationBuilder.Sql(@"drop function on_transaction_delete");
             migrationBuilder.Sql(@"drop function on_transaction_update");
             migrationBuilder.Sql(@"drop function on_account_update");
-            migrationBuilder.Sql(@"drop view ""TransactionBalances""");
+            migrationBuilder.Sql(@"drop materialized view ""TransactionBalances""");
 
             migrationBuilder.CreateTable(
                 name: "TransactionBalances",
@@ -31,8 +31,137 @@ namespace dal.Migrations
                         column: x => x.Id,
                         principalTable: "Transactions",
                         principalColumn: "Id",
-                        onDelete: ReferentialAction.Restrict);
+                        onDelete: ReferentialAction.Cascade);
                 });
+
+            migrationBuilder.Sql(@"create or replace procedure update_balances(account_id int, refresh_date timestamp without time zone)
+  language plpgsql
+as $$
+  begin
+	
+	if account_id is null then
+		RAISE EXCEPTION 'Account id must be set'
+      	USING ERRCODE = 10001; 
+	end if;
+	
+	delete from ""TransactionBalances"" b using ""Transactions"" t
+	where b.""Id"" = t.""Id"" 
+	and t.""AccountId"" = account_id
+	and (refresh_date is null or t.""Date"" >= refresh_date);
+
+	insert into ""TransactionBalances""
+	select
+	  t.""Id"",
+	  (select sum(t2.""Amount"")
+		from ""Transactions"" t2 
+		where t2.""Date"" <= t.""Date"" and t2.""AccountId"" = t.""AccountId"" ) + a.""InitialBalance"" as ""Balance""
+	from ""Transactions"" t 
+	inner join ""Accounts"" a on a.""Id"" = t.""AccountId""
+	where t.""AccountId"" = account_id
+	and (refresh_date is null or t.""Date"" >= refresh_date)
+	order by t.""AccountId"", t.""Date"" desc;
+
+  end;
+$$;
+");
+
+            migrationBuilder.Sql(@"create or replace procedure refresh_all_balances()
+  language plpgsql
+as $$
+  begin
+
+	delete from ""TransactionBalances"";
+
+	insert into ""TransactionBalances""
+	select
+	  t.""Id"",
+	  (select sum(t2.""Amount"")
+		from ""Transactions"" t2 
+		where t2.""Date"" <= t.""Date"" and t2.""AccountId"" = t.""AccountId"" ) + a.""InitialBalance"" as ""Balance""
+	from ""Transactions"" t 
+	inner join ""Accounts"" a on a.""Id"" = t.""AccountId""
+	order by t.""AccountId"", t.""Date"" desc;
+
+  end;
+$$;
+");
+
+            migrationBuilder.Sql(@"
+create function on_transaction_insert() returns trigger
+  security definer
+  language plpgsql
+as $$
+  begin
+    call update_balances(new.""AccountId"", new.""Date"");
+    return null;
+  end;
+$$;
+
+create trigger transaction_insert after insert on ""Transactions""
+    for each row execute procedure on_transaction_insert();
+");
+
+            migrationBuilder.Sql(@"
+create function on_transaction_delete() returns trigger
+  security definer
+  language plpgsql
+as $$
+  begin
+    call update_balances(old.""AccountId"", old.""Date"");
+    return null;
+  end;
+$$;
+
+create trigger transaction_delete after delete on ""Transactions""
+    for each row execute procedure on_transaction_delete();");
+
+            migrationBuilder.Sql(@"
+create or replace function on_transaction_update() returns trigger
+  security definer
+  language plpgsql
+as $$
+  declare dt timestamp without time zone;
+  begin
+
+      if old.""AccountId"" != new.""AccountId"" then
+        call update_balances(old.""AccountId"", old.""Date"");
+        call update_balances(new.""AccountId"", new.""Date"");
+      else 
+	  	if old.""Amount"" != new.""Amount"" or old.""Date"" != new.""Date"" then
+			if old.""Date"" > new.""Date"" then
+				dt = new.""Date"";
+			else
+				dt = old.""Date"";
+			end if;
+
+			call update_balances(old.""AccountId"", dt);
+		end if;
+      end if;
+      return null;
+	  
+  end;
+$$;
+
+create trigger transaction_update after update on ""Transactions""
+    for each row execute procedure on_transaction_update();");
+
+            migrationBuilder.Sql(@"
+create function on_account_update() returns trigger
+  security definer
+  language plpgsql
+as $$
+  begin
+      if old.""InitialBalance"" != new.""InitialBalance"" then
+        call update_balances(old.""Id"", NULL);
+      end if;
+      return null;
+  end;
+$$;
+
+create trigger account_update after update on ""Accounts""
+    for each row execute procedure on_account_update();");
+
+            migrationBuilder.Sql("call refresh_all_balances();");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
@@ -45,6 +174,8 @@ namespace dal.Migrations
             migrationBuilder.Sql(@"drop function on_transaction_delete");
             migrationBuilder.Sql(@"drop function on_transaction_update");
             migrationBuilder.Sql(@"drop function on_account_update");
+            migrationBuilder.Sql(@"drop procedure update_balances");
+            migrationBuilder.Sql(@"drop procedure refresh_all_balances");
             migrationBuilder.DropTable(
                 name: "TransactionBalances");
 
@@ -105,7 +236,7 @@ as $$
 $$;
 
 create trigger transaction_update after update on ""Transactions""
-    for each row execute procedure on_transaction_update();");    
+    for each row execute procedure on_transaction_update();");
 
             migrationBuilder.Sql(@"
 create function on_account_update() returns trigger
